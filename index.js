b44fc5e5c6cae3793b4bbb587f9f2848dfa6a967
manuel <manuel@sahores.com>
2020-08-22 18:02:17 -0300

    First commit

---
+++ index.js
@@ -0,0 +1,20 @@
+const express = require('express');
+const mongoose = require('mongoose');
+const cors = require('cors');
+require("dotenv").config();
+
+// set up express
+const app = express();
+app.use(express.json());
+app.use(cors());
+
+const PORT = process.env.PORT || 6000;
+app.listen(PORT, () => console.log("The server has starten on port " + PORT));
+
+mongoose.connect(process.env.MONGODB_CONNECTION_STRING, {
+  useNewUrlParser: true,
+  useUnifiedTopology: true, 
+}, (err) => {
+  if(err) throw err;
+  console.log("MONGO DB connection established")
+})
