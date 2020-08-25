9ac88d445c264043aaa3e80725f5e601879760aa
manuel <manuel@sahores.com>
2020-08-25 20:06:56 -0300

    Routes and middlewares

--- index.js
+++ index.js
@@ -8,13 +8,17 @@ const app = express();
 app.use(express.json());
 app.use(cors());
 
-const PORT = process.env.PORT || 6000;
+const PORT = process.env.PORT || 5000;
 app.listen(PORT, () => console.log("The server has starten on port " + PORT));
 
 mongoose.connect(process.env.MONGODB_CONNECTION_STRING, {
   useNewUrlParser: true,
   useUnifiedTopology: true, 
+  useCreateIndex: true
 }, (err) => {
   if(err) throw err;
   console.log("MONGO DB connection established")
 })
+
+// routes
+app.use("/users", require("./routes/userRouter")); 
